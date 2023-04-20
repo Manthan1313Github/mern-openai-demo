3b97ee9c73c67c123f16097db9aede0cd404d227
Manthan Parmar <parmarmanthan1313@gmail.com>
2023-04-20 17:01:35 +0530

    Add sidebar and main section with style

--- chatgpt-clone/src/App.js
+++ chatgpt-clone/src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <section className="sidebar">
+        <button>New chat</button>
+        <ul className="history">
+          <li>first item</li>
+        </ul>
+        <nav>
+          <p>history</p>
+        </nav>
+      </section>
+      <section className="main">
+        <h1>Chatgpt React Clone</h1>
+        <ul className="top-section"></ul>
+        <div className="bottom-section">
+          <div className="input-container">
+            <input />
+            <div id="submit">
+              <p className="fa fa-angle-double-right"></p>
+            </div>
+          </div>
+          <p className="metadata">Chat GPT version - 14</p>
+        </div>
+      </section>
     </div>
   );
 }
